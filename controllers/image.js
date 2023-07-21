574b757decf109a0376d9401119786b41ac80915
Gagandeep <deepgagan9956@gmail.com>
2023-07-21 16:41:20 +0530

    face api

---
+++ controllers/image.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const handleApiCall = (req, res) => {
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": "clarifai",
+            "app_id": "main"
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": req.body.input
+                    }
+                }
+            }
+        ]
+    });
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + '22dc7c5d60e64736ac415534c0448624'
+        },
+        body: raw
+    };
+    fetch(`https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('error getting count'))
+}
+module.exports = {
+    handleImage,
+    handleApiCall
+};
---
+++ script.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const saltRounds = 10;
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+const knex = require('knex');
+const db = knex({
+    client: 'pg',
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
+});
+const app = express();
+
+app.use(cors());
+app.use(bodyParse.json());
+app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt,saltRounds)})
+app.get('/profile/:id', (req,res)=>{profile.handleProfile(req,res,db)})
+app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
