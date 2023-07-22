f462e2815474dcea3f1e2900b0768c2c5606ab31
Gagandeep <deepgagan9956@gmail.com>
2023-07-22 14:30:25 +0530

    finally

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const bodyParse = require('body-parser');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
-const saltRounds = 10;
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
-const knex = require('knex');
-const dotenv = require('dotenv');
-const { Pool } = require('pg');
-dotenv.config();
-const db = new Pool({
-    connectionString: process.env.DBConfigLink,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 const app = express();
+const route = require('./router/route')
 
 app.use(cors());
 app.use(bodyParse.json());
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
-app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt,saltRounds)})
-app.get('/profile/:id', (req,res)=>{profile.handleProfile(req,res,db)})
-app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
-
+app.use('/',route)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
