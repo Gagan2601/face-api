251951132a8369e68d119abac2529e7052452a16
Gagandeep <deepgagan9956@gmail.com>
2023-07-22 14:35:25 +0530

    final

--- router/route.js
+++ router/route.js
@@ -1,10 +1,10 @@
 const saltRounds = 10;
 const express = require('express');
 const app = express();
-const register = require('./controllers/register')
-const signin = require('./controllers/signin')
-const profile = require('./controllers/profile')
-const image = require('./controllers/image')
+const register = require('../controllers/register')
+const signin = require('../controllers/signin')
+const profile = require('../controllers/profile')
+const image = require('../controllers/image')
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 const { Pool } = require('pg');
