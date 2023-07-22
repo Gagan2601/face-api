76361bb15ddbf6ba8a262deb186ce9a4de18d00f
Gagandeep <deepgagan9956@gmail.com>
2023-07-22 13:18:31 +0530

    finally

--- script.js
+++ script.js
@@ -27,6 +27,10 @@ app.get('/profile/:id', (req,res)=>{profile.handleProfile(req,res,db)})
 app.put('/image', (req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
 app.listen(process.env.PORT || 5000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 });
